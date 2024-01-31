103857293fc5cfd891a382b16e722f31612747cf
adam_kinyua <85613991+AdamKinyua@users.noreply.github.com>
2024-01-31 10:51:20 -0500

    updating

---
+++ canonical_orbitals_plots/analysis.py
@@ -0,0 +1,78 @@
+from numpy import *
+import matplotlib.pyplot as plt
+
+
+def calculate_complex_squared_magnitude(file_path):
+    results = []
+
+    # Open the file and read each line
+    with open(file_path, 'r') as file:
+        for line in file:
+            columns = line.split()
+            if len(columns) == 3:
+                real_part = float(columns[1])
+                imag_part = float(columns[2][:-1])  # Remove the 'i' at the end
+                complex_number = complex(real_part, imag_part)
+                # Calculate the squared magnitude and append to the list
+                z_z = complex_squared_magnitude(complex_number.real, complex_number.imag)
+                results.append(z_z)
+            else:
+                value = float(columns[0])
+                value_squared = squared_value(value)
+                results.append(value_squared)
+    return results
+
+
+def complex_squared_magnitude(a, b):
+    # z*z = sqrt(a**2+b**2)
+    result_real = a ** 2 + b ** 2
+    result = sqrt(result_real)
+    return result
+
+
+def squared_value(a):
+    aa = a ** 2
+    return aa
+
+
+def x_values_(file):
+    x_vals = []
+    with open(file, 'r') as file:
+        for line in file:
+            columns = line.split()
+            if len(columns) == 1:
+                val = float(columns[0])
+                x_vals.append(val)
+    return x_vals
+
+
+file1 = 'homo'
+file2 = 'lumo'
+file3 = 'lumo+1'
+file4 = 'lumo+2'
+homo = calculate_complex_squared_magnitude(file1)
+lumo = calculate_complex_squared_magnitude(file2)
+lumo1 = calculate_complex_squared_magnitude(file3)
+lumo2 = calculate_complex_squared_magnitude(file4)
+
+print(lumo)
+print(len(homo))
+print(len(lumo))
+print(len(lumo1))
+print(len(lumo2))
+
+
+x_values = x_values_('x_values')
+plt.plot(x_values, homo)
+plt.plot(x_values, lumo)
+plt.plot(x_values, lumo1)
+plt.plot(x_values, lumo2)
+# plt.ylim(0.9999998, 1.0000005)
+plt.gca().yaxis.set_major_formatter(plt.FormatStrFormatter('%.3f'))
+plt.gca().set_ylim(-0.1, 1.15)  # Set y-axis limits on the current axis
+
+plt.xlabel('pt charge distance along z-axis(Angstroms)')
+plt.ylabel('Weight')
+plt.title('H$_2$ Stationary Approximation Canonical Orbitals Plots')  # , LUMO, LUMO+1, LUMO+2))
+plt.legend([r'|<homo$_0$|S|homo$_d$>|$^2$', r'|<homo$_0$|S|lumo$_d$>|$^2$', r'|<homo$_0$|S|lumo+1$_d$>|$^2$', r'|<homo$_0$|S|lumo+2$_d$>|$^2$'])
+plt.show()
